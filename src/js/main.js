27afed99b9bd6fb2ff6bd4b7e9f0ec1ec972c62c
Dimitar Danov <dimitardanov@users.noreply.github.com>
2017-09-17 05:34:43 +0300

    Substituted style changes with render funcs for welcome face

--- src/js/main.js
+++ src/js/main.js
@@ -2,14 +2,20 @@
 window.jQuery = $ = require('jquery');
 var bootstrap = require('bootstrap-sass');
 var data = require('./data/data.js');
+var render = require('./renderers/render.js');
 
 var content = document.querySelector('.content');
 var header = document.querySelector('#header');
 var footer = document.querySelector('#footer');
 
-(function showTimerFace() {
-  [content, header, footer].forEach(function(el) {
-    el.style.display = 'block';
-  });
-})();
+render.settingsBtn(header);
+render.timer(
+  content,
+  {
+    minutes: ('0' + data.data.timer.minutes).slice(-2),
+    seconds: ('0' + data.data.timer.seconds).slice(-2)
+  }
+);
+render.playBtn(footer);
+
 });
---
+++ src/js/renderers/render.js
@@ -0,0 +1,26 @@
+
+var btnSettingsTempl = require('../templates/button-settings.hbs');
+var btnPlayTempl = require('../templates/button-play.hbs');
+var timerTempl = require('../templates/timer.hbs');
+
+function renderItem(parent, template, data) {
+  parent.innerHTML = template(data);
+}
+
+function settingsBtn(parent) {
+  renderItem(parent, btnSettingsTempl, {});
+}
+
+function playBtn(parent) {
+  renderItem(parent, btnPlayTempl, {});
+}
+
+function timer(parent, data) {
+  renderItem(parent, timerTempl, data);
+}
+
+module.exports = {
+  settingsBtn: settingsBtn,
+  playBtn: playBtn,
+  timer: timer
+};
