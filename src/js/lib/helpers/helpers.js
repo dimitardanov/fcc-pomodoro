441bc9eed05863cf4ed7280858da8829a5b1074a
Dimitar Danov <dimitardanov@users.noreply.github.com>
2017-09-22 10:46:26 +0300

    Added helper funcs for angle <-> value manipulation

--- src/js/lib/helpers/helpers.js
+++ src/js/lib/helpers/helpers.js
@@ -25,7 +25,61 @@ function getBtnIndices(dind, numItems) {
   };
 }
 
+function transformCoords(origin, pos) {
+  return {
+    x: pos.x - origin.x,
+    y: -(pos.y - origin.y)
+  };
+}
+
+function calcAngle(pos) {
+  var angle = Math.atan2(pos.y, pos.x);
+  angle = (angle >= 0) ? angle : 2 * Math.PI + angle;
+  return angle;
+}
+
+function transformAngle(angle) {
+  // returns the equivalent angle counted clockwise from the +Y-axis
+  if (angle <= Math.PI / 2) {
+    angle = Math.PI / 2 - angle;
+  } else {
+    angle = 5 / 2 * Math.PI - angle;
+  }
+  return angle;
+}
+
+function rad2deg(rad) {
+  return 360 * rad / (2 * Math.PI);
+}
+
+function val2deg(val, maxVal) {
+  return val / maxVal * 360;
+}
+
+function deg2val(deg, maxVal) {
+  return deg / 360 * maxVal;
+}
+
+function getAngle(event) {
+  var pos = {
+    x: event.pageX,
+    y: event.pageY
+  };
+  var coords = transformCoords(getOrigin(), pos);
+  var angle = calcAngle(coords);
+  var trAngle = transformAngle(angle);
+  return rad2deg(trAngle);
+}
+
 module.exports = {
+  getOrigin: getOrigin,
+  transformCoords: transformCoords,
+  calcAngle: calcAngle,
+  transformAngle: transformAngle,
+  rad2deg: rad2deg,
+  val2deg: val2deg,
+  deg2val: deg2val,
+  getAngle: getAngle,
   calcMS: calcMS,
   convertToMinSec: convertToMinSec,
   getBtnIndices: getBtnIndices
--- src/js/test/helpers.test.js
+++ src/js/test/helpers.test.js
@@ -60,3 +60,154 @@ describe('getBtnIndices function', function() {
   });
 });
 
+describe('transformCoords function', function() {
+  before(function() {
+    this.originZero = {x: 0, y: 0};
+    this.origin1 = {x: 5, y: 10};
+  });
+
+  after(function() {
+    delete this.originZero;
+    delete this.origin1;
+  });
+
+  it('should return an object with "x" and "y" keys', function() {
+    var coords = h.transformCoords(this.originZero, this.originZero);
+    expect(coords).to.have.ownProperty('x');
+    expect(coords).to.have.ownProperty('y');
+    expect(Object.keys(coords).length).to.equal(2);
+  });
+
+  it('should return 0,0 position if given 0,0 position and origin', function() {
+    var coords = h.transformCoords(this.originZero, this.originZero);
+    expect(coords.x).to.equal(0);
+    expect(coords.y).to.equal(0);
+  });
+
+  it('should return transformed coords if in quadrant 1', function() {
+    var coords = h.transformCoords(this.origin1, {x: 8, y: 8});
+    expect(coords).to.deep.equal({x: 3, y: 2});
+  });
+
+  it('should return transformed coords if in quadrant 2', function() {
+    var coords = h.transformCoords(this.origin1, {x: 1, y: 2});
+    expect(coords).to.deep.equal({x: -4, y: 8});
+  });
+
+  it('should return transformed coords if in quadrant 3', function() {
+    var coords = h.transformCoords(this.origin1, {x: 2, y: 14});
+    expect(coords).to.deep.equal({x: -3, y: -4});
+  });
+
+  it('should return transformed coords if in quadrant 4', function() {
+    var coords = h.transformCoords(this.origin1, {x: 9, y: 25});
+    expect(coords).to.deep.equal({x: 4, y: -15});
+  });
+});
+
+describe('calcAngle function', function() {
+  it('should return 0 if point on the positive X-axis', function() {
+    expect(h.calcAngle({x: 2, y: 0})).to.equal(0);
+  });
+
+  it('should return 1/2*PI if point on the +Y-axis', function() {
+    expect(h.calcAngle({x: 0, y: 1})).to.equal(1 / 2 * Math.PI);
+  });
+
+  it('should return 3/2*PI if point on the -Y-axis', function() {
+    expect(h.calcAngle({x: 0, y: -1})).to.equal(3 / 2 * Math.PI);
+  });
+
+  it('should return the angle with +X-axis of point in Q1', function() {
+    expect(h.calcAngle({x: 2, y: 2})).to.equal(Math.PI / 4);
+  });
+
+  it('should return the angle with +X-axis of point in Q2', function() {
+    expect(h.calcAngle({x: -3, y: 3})).to.equal(3 / 4 * Math.PI);
+  });
+
+  it('should return the angle with +X-axis of point in Q3', function() {
+    expect(h.calcAngle({x: -1, y: -1})).to.equal(5 / 4 * Math.PI);
+  });
+
+  it('should return the angle with +X-axis of point in Q4', function() {
+    expect(h.calcAngle({x: 5, y: -5})).to.equal(7 / 4 * Math.PI);
+  });
+});
+
+describe('transformAngle function', function() {
+  it('should return 1/2*PI for 0', function() {
+    expect(h.transformAngle(0)).to.equal(1 / 2 * Math.PI);
+  });
+
+  it('should return 1/4*PI for 1/4*PI', function() {
+    expect(h.transformAngle(1 / 4 * Math.PI)).to.equal(1 / 4 * Math.PI);
+  });
+
+  it('should return 0 for 1/2*PI', function() {
+    expect(h.transformAngle(1 / 2 * Math.PI)).to.equal(0);
+  });
+
+  it('should return 7/4*PI for 3/4*PI', function() {
+    expect(h.transformAngle(3 / 4 * Math.PI)).to.equal(7 / 4 * Math.PI);
+  });
+
+  it('should return 3/2*PI for PI', function() {
+    expect(h.transformAngle(Math.PI)).to.equal(3 / 2 * Math.PI);
+  });
+
+  it('should return 5/4*PI for 5/4*PI', function() {
+    expect(h.transformAngle(5 / 4 * Math.PI)).to.equal(5 / 4 * Math.PI);
+  });
+
+  it('should return PI for 3/2*PI', function() {
+    expect(h.transformAngle(3 / 2 * Math.PI)).to.equal(Math.PI);
+  });
+
+  it('should return 3/4*PI for 7/4*PI', function() {
+    expect(h.transformAngle(7 / 4 * Math.PI)).to.equal(3 / 4 * Math.PI);
+  });
+
+  it('should return 4/3*PI for 7/6*PI', function() {
+    expect(h.transformAngle(7 / 6 * Math.PI)).to.equal(4 / 3 * Math.PI);
+  });
+
+  it('should return 4/6*PI for 11/6*PI', function() {
+    expect(
+      h.transformAngle(11 / 6 * Math.PI)).to.be.closeTo(4 / 6 * Math.PI, 1e-10);
+  });
+});
+
+describe('rad2deg function', function() {
+  it('should return 0 for 0', function() {
+    expect(h.rad2deg(0)).to.equal(0);
+  });
+
+  it('should return 45 for PI/4', function() {
+    expect(h.rad2deg(Math.PI / 4)).to.equal(45);
+  });
+
+  it('should return 330 for 11/6PI', function() {
+    expect(h.rad2deg(11 / 6 * Math.PI)).to.be.closeTo(330, 1e-10);
+  });
+});
+
+describe('val2deg function', function() {
+  it('should return 0 for 0 val', function() {
+    expect(h.val2deg(0, 120)).to.equal(0);
+  });
+
+  it('should return a proportional angle to the val/maxVal', function() {
+    expect(h.val2deg(23, 230)).to.equal(36);
+  });
+});
+
+describe('deg2val function', function() {
+  it('should return 0 for 0deg', function() {
+    expect(h.deg2val(0, 23)).to.equal(0);
+  });
+
+  it('should return a proportional value to the angle', function() {
+    expect(h.deg2val(36, 230)).to.equal(23);
+  });
+});
